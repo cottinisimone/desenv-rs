8734bf9b70d8febf78a6213251534a3fed31fefe
Simone Cottini <cottini.simone@gmail.com>
2022-11-11 18:35:23 +0100

    main Desenv trait; primitive macro

---
+++ desenv_macros/src/lib.rs
@@ -0,0 +1,47 @@
+use proc_macro::TokenStream;
+
+use proc_macro2::{Ident, Span};
+use quote::quote;
+use syn::spanned::Spanned;
+use syn::{DeriveInput, Error, Fields};
+
+#[proc_macro_derive(Desenv, attributes(desenv))]
+pub fn derive_desenv(input: TokenStream) -> TokenStream {
+    let derive_input: DeriveInput = syn::parse_macro_input!(input as syn::DeriveInput);
+
+    // Create a new span to get expansion information
+    let derive_input_span: Span = derive_input.span();
+
+    let derivation_result: Result<proc_macro2::TokenStream, Error> = match derive_input.data {
+        // Only non-tuple structs are allowed
+        syn::Data::Struct(ref data_struct) => match data_struct.fields {
+            // Only structs with named fields are allowed
+            Fields::Named(ref _fields) => {
+                let struct_name: &Ident = &derive_input.ident;
+                Ok(quote! {
+                    impl Desenv for #struct_name {
+                        fn load() -> Result<Self, ::desenv::Error>
+                        where
+                            Self: Sized,
+                        {
+                            todo!()
+                        }
+                    }
+                })
+            }
+            _ => Err(Error::new(
+                derive_input_span,
+                "desenv could be derived only on structs with named fields",
+            )),
+        },
+        _ => Err(Error::new(
+            derive_input_span,
+            "desenv could be derived only on non-tuple struct types",
+        )),
+    };
+
+    match derivation_result {
+        Ok(tokens) => tokens.into(),
+        Err(err) => err.into_compile_error().into(),
+    }
+}
