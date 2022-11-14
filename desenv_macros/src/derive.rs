083a9f30076e9729d7b37ae857a4464cd7525f2e
Simone Cottini <cottini.simone@gmail.com>
2022-11-14 17:39:49 +0100

    simple parser + prefix and nested

--- desenv_macros/src/derive.rs
+++ desenv_macros/src/derive.rs
@@ -36,13 +36,48 @@ fn expand_fields(
 
     for field in fields {
         let field_attr: attr::Field = attr::Field::from_attrs(&field.attrs, field.span())?;
-        expanded_fields.push(expand_field(field, &field_attr, struct_attr));
+        expanded_fields.push(expand_field(field, &field_attr, struct_attr)?);
     }
 
     Ok(expanded_fields)
 }
 
-fn expand_field(field: &syn::Field, _field_attr: &attr::Field, _struct_attr: &attr::Struct) -> TokenStream {
+fn expand_field(
+    field: &syn::Field,
+    field_attr: &attr::Field,
+    struct_attr: &attr::Struct,
+) -> Result<TokenStream, Error> {
     let field_ident: &Option<Ident> = &field.ident;
-    quote!(#field_ident: "".to_string())
+
+    if field_attr.nested {
+        let field_type: &syn::Type = &field.ty;
+        Ok(quote!(#field_ident: <#field_type>::_load()?))
+    } else {
+        let field_identity_as_string: String = field
+            .ident
+            .as_ref()
+            .map(ToString::to_string)
+            .ok_or_else(|| Error::new(field.span(), "failed to stringify identity"))?;
+        let prefix: String = struct_attr.get_prefix();
+
+        let var_name: TokenStream = var_name(field_identity_as_string.as_str(), prefix.as_str(), field_attr);
+        let token_stream: TokenStream = quote_field(&var_name, field_attr);
+        Ok(quote!(#field_ident: #token_stream))
+    }
+}
+
+fn quote_field(var_name: &TokenStream, _field_attr: &attr::Field) -> TokenStream {
+    quote!(std::env::var(#var_name.as_str()).unwrap().parse().unwrap())
+}
+
+// Returns the environment variable name that should be fetched. If could be the field name upcased
+// or the rename value (both prefixed).
+fn var_name(field_name: &str, prefix: &str, field_attr: &attr::Field) -> TokenStream {
+    let var_name: String = if let attr::Field { rename: Some(rename), .. } = field_attr {
+        rename.to_string()
+    } else {
+        field_name.to_uppercase()
+    };
+
+    quote!(format!("{}{}", #prefix, #var_name))
 }
--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -8,8 +8,54 @@ mod tests {
     }
 
     #[test]
-    fn could_compile() {
+    fn works() {
+        std::env::set_var("FIELD", "value");
         let c: Config = desenv::load().unwrap();
-        assert!(c.field.is_empty());
+        assert_eq!(c.field, "value");
+    }
+
+    #[derive(Desenv)]
+    pub struct Config2 {
+        field: i32,
+    }
+
+    #[test]
+    fn works_with_i32() {
+        std::env::set_var("FIELD", 10.to_string());
+        let c: Config2 = desenv::load().unwrap();
+        assert_eq!(c.field, 10);
+    }
+
+    #[derive(Desenv)]
+    #[desenv(prefix = "PREFIX_")]
+    pub struct Config3 {
+        field: i32,
+    }
+
+    #[test]
+    fn works_with_i32_and_prefix() {
+        std::env::set_var("PREFIX_FIELD", 10.to_string());
+        let c: Config3 = desenv::load().unwrap();
+        assert_eq!(c.field, 10);
+    }
+
+    #[derive(Desenv)]
+    #[desenv(prefix = "PREFIX_")]
+    pub struct Config4 {
+        #[desenv(nested)]
+        field: Nested4,
+    }
+
+    #[derive(Desenv)]
+    pub struct Nested4 {
+        field: String,
+    }
+
+    #[test]
+    fn works_with_nested() {
+        // TODO: this should be PREFIX_FIELD anyway
+        std::env::set_var("FIELD", "value");
+        let c: Config4 = desenv::load().unwrap();
+        assert_eq!(c.field.field, "value");
     }
 }
