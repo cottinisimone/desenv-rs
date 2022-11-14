bcbe63d0a5e6b2d9793a3c65bc50bcd3607e57b7
Simone Cottini <cottini.simone@gmail.com>
2022-11-14 17:33:14 +0100

    implement std::error::Error trait for Error

--- desenv/src/error.rs
+++ desenv/src/error.rs
@@ -1,9 +1,16 @@
-use std::fmt::{Debug, Formatter, Result};
+use std::fmt::{Debug, Display, Formatter, Result};
 
+#[derive(Debug)]
 pub enum Error {}
 
-impl Debug for Error {
+impl Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         write!(f, "Error")
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
     }
 }
