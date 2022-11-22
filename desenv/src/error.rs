a3a218866d73f5afe1da137233be9c4fadb79783
Simone Cottini <cottini.simone@gmail.com>
2022-11-22 17:33:15 +0100

    in-code documentation

--- desenv/src/error.rs
+++ desenv/src/error.rs
@@ -1,5 +1,12 @@
 use std::fmt::{Debug, Display, Formatter, Result};
 
+/// All possible errors returned by this library after the [`desenv::load`] call. The errors are:
+///
+/// - `MissingVar`: the environment variable could not be found
+/// - `NotUnicodeVar`: the environment variable exists but is not UTF-8 encoded.
+/// - `ParseFromStr`: the environment variable contains a value that cannot be parsed to original value.
+/// - `Custom`: custom error defined by the user of this library. Is mainly used to handle the errors
+///             of a custom deserializable type (mixing in the [`std::str::FromStr`] trait).
 pub enum Error {
     MissingVar(String),
     NotUnicodeVar(String),
--- desenv/src/lib.rs
+++ desenv/src/lib.rs
@@ -1,12 +1,21 @@
+//! Desenv-rs is a library used to deserialize the environment variables into a given struct deriving
+//! `Desenv` macro.
+
 pub use desenv_macros::Desenv;
 pub use error::Error;
 
 mod error;
 
-/// Load the configuration
+/// Load all the environment variables into a given `Desenv` struct.
 ///
 /// # Errors
-/// -
+///
+/// Will return `Err` if:
+/// - One environment variable for non-optional field is missing (and no default is set).
+/// - Deserialization from string of the resulting type fails.
+/// - Deserialization of default value from string to resulting type fails.
+/// - Both environment variable for non-optional field and default environment variable
+///   is missing.
 pub fn load<T>() -> Result<T, Error>
 where
     T: Desenv,
@@ -15,10 +24,17 @@ where
 }
 
 pub trait Desenv {
-    /// Load the configuration
+    /// Load the configuration with the given optional `parent_prefix`.
+    /// DO NOT USE THIS FUNCTION! Use [`desenv::load`] instead!
     ///
     /// # Errors
-    /// -
+    ///
+    /// Will return `Err` if:
+    /// - One environment variable for non-optional field is missing (and no default is set).
+    /// - Deserialization from string of the resulting type fails.
+    /// - Deserialization of default value from string to resulting type fails.
+    /// - Both environment variable for non-optional field and default environment variable
+    ///   is missing.
     fn _load(parent_prefix: Option<String>) -> Result<Self, Error>
     where
         Self: Sized;
